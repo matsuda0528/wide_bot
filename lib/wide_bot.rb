139b32441b124974eeef2fa770e1761bf77cb401
rikuto matsuda <p99y92og@s.okayama-u.ac.jp>
2022-05-01 01:53:19 +0900

    Initial commit

---
+++ lib/wide_bot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wide_bot/version"
+
+module WideBot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wide_bot/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WideBot
+  VERSION = "0.1.0"
+end
