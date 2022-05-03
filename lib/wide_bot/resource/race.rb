2a0b15190637d03931d9d4a5811b17f8091d7a66
rikuto matsuda <p99y92og@s.okayama-u.ac.jp>
2022-05-03 23:34:55 +0900

    Update race class to set params

--- lib/wide_bot/resource/race.rb
+++ lib/wide_bot/resource/race.rb
@@ -1,6 +1,12 @@
 module WideBot
   class Race
-    def initialize
+    def initialize(name:, grade:, race_course:, distance:, turf_or_dirt:, horses:)
+      @name = name
+      @grade = grade
+      @race_course = race_course
+      @distance = distance
+      @turf_or_dirt = turf_or_dirt
+      @horses = horses
     end
   end
 end
