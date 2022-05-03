2885b1143d39e95d975f38da9e79b7bfc8824c9a
rikuto matsuda <p99y92og@s.okayama-u.ac.jp>
2022-05-03 23:38:31 +0900

    Update horse class to set params

--- lib/wide_bot/resource/horse.rb
+++ lib/wide_bot/resource/horse.rb
@@ -1,6 +1,11 @@
 module WideBot
   class Horse
-    def initialize
+    def initialize(name:, age:, gender:, jockey:, barn:)
+      @name = name
+      @age = age
+      @gender = gender
+      @jockey = jockey
+      @barn = barn #厩舎
     end
   end
 end
