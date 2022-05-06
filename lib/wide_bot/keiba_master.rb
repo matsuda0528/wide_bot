e70b1034aca1c457999067fb0b934f2f8bed4fb6
rikuto matsuda <p99y92og@s.okayama-u.ac.jp>
2022-05-06 18:00:03 +0900

    Add choose_two_horses_by_random method

--- lib/wide_bot/keiba_master.rb
+++ lib/wide_bot/keiba_master.rb
@@ -4,6 +4,13 @@ module WideBot
     end
 
     def predict_race(race_info)
+      choose_two_horses_by_random(race_info.horses)
+    end
+
+    private
+
+    def choose_two_horses_by_random(horses)
+      horses.sample 2
     end
   end
 end
