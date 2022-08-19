f0de1164506e5dea7bdc203d5c395feff9e171ca
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2022-08-19 17:03:30 +0900

    Update JRACalendarHTMLParser to implement parse method

--- lib/wide_bot/service/jra/jra_calendar_html_parser.rb
+++ lib/wide_bot/service/jra/jra_calendar_html_parser.rb
@@ -8,8 +8,18 @@ module WideBot
     def parse(html)
       doc = Nokogiri::HTML.parse(html)
 
-      binding.pry
-      # return array(race id)
+      kaisai_year = doc.xpath("//div[@class='contents_header opt']").xpath(".//div[@class='main']").text.slice(0..3)
+      kaisai_infos = doc.xpath("//div[@id='program_list']").xpath(".//div[@class='main']").map(&:text)
+
+      race_ids = []
+      kaisai_infos.map do |info|
+        times, place, day = info.scan(/.{1,2}/)
+        code = RaceCourseIDTable[place]
+        times = format("%02d", times.to_i)
+        day = format("%02d", day.to_i)
+        race_ids.append RaceId.new(year: kaisai_year, code: code, times: times, day: day)
+      end
+      return race_ids
     end
   end
 end
